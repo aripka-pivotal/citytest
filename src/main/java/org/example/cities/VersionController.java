0394ebbb4c071c1a020386b32fc2a355ff49ed38
Andrew Ripka <aripka@pivotal.io>
2015-03-10 14:25:50 -0500

    update version

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Cincinnati Cloud Platform Roadshow! - v3";
+        return "Greetings Cincinnati Cloud Platform Roadshow! - v4";
     }
 }
